bdbc03035cdad02f8e0f132a4798829d8ef66264
Simenhd <simenhdale@gmail.com>
2025-09-23 22:29:05 +0200

    1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,52 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const handleVote = () => {
-    const copy = [...votes]
-    copy[selected] += 1
-    setVotes(copy)
-    console.log(votes)
-  }
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
 
   const handleNextAnectode = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
-  
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const mostVotedIndex = votes.indexOf(Math.max(...votes));
 
   return (
     <div>
+      <div style = {{fontSize: "28px", fontWeight:"700", paddingBottom: "10px"}}>Anecdote of the day</div>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
       <div>
-        {anecdotes[selected]}
+        <button onClick={() => handleVote()}>vote</button>
+        <button onClick={() => handleNextAnectode()}>next anecdote</button>
       </div>
-      <div>
-        has {votes[selected]} votes
-      </div>
-      <div>
-        <button onClick={() => handleVote()
-        }>
-          vote
-        </button>
-        <button onClick={() => handleNextAnectode()}>
-          next anecdote
-        </button>
-      </div>
+      <div style = {{fontSize: "28px", fontWeight:"700", paddingTop: "10px"}}>Anecdote with the most votes</div>
+      <div>{anecdotes[mostVotedIndex]}</div>
+      <div>has {votes[mostVotedIndex]} votes</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
