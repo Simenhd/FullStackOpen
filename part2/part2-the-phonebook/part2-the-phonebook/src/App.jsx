e736654fbc6593748f9a3d2baea84cd59e938752
Simenhd <simenhdale@gmail.com>
2025-10-15 20:42:49 +0200

    2.8

--- part2/part2-the-phonebook/part2-the-phonebook/src/App.jsx
+++ part2/part2-the-phonebook/part2-the-phonebook/src/App.jsx
@@ -2,33 +2,46 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: '1' },
+    { name: 'Arto Hellas', number: '22489015', id: '1' },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   const addName = event => {
     event.preventDefault()
     const nameObject= {
       name: newName,
+      number: newNumber,
       id: String(persons.length + 1)
     }
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      
+    } else if (persons.some(person => person.number === newNumber)) {
+      alert(`${newNumber} is already added to phonebook`)
+      setNewNumber('')
       return
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <div>debug: {newName}</div>
+      <div>debug: {newNumber}</div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
@@ -36,6 +49,12 @@ const App = () => {
             value={newName}
             onChange = {handleNameChange}
           />
+        <div>
+          number: <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -44,7 +63,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => 
-          <li key={person.id}>{person.name}</li>  
+          <li key={person.id}>{person.name} {person.number}</li>  
         )}
       </ul>
     </div>
