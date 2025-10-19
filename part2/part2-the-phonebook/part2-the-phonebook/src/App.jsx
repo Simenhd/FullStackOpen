52bb7d35f655d459d17b3d3d256840b223ebc244
Simenhd <simenhdale@gmail.com>
2025-10-19 17:18:16 +0200

    2.11

--- part2/part2-the-phonebook/part2-the-phonebook/src/App.jsx
+++ part2/part2-the-phonebook/part2-the-phonebook/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+    const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+
+  console.log('render', persons.length, 'notes')
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -72,7 +78,7 @@ const App = () => {
         onSubmit={addName}
       />
       <h3>Numbers</h3>
-      <Persons persons={personsToShow}/>
+      <Persons persons={personsToShow} />
     </div>
   );
 };
